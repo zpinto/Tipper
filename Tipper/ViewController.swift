a6749184fe0221d245adaf371e5e3bed1cf0d63a
Zachary Pinto <zpinto@uci.edu>
2020-01-03 14:48:47 -0800

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Zachary Pinto on 1/3/20.
+//  Copyright © 2020 Zachary Pinto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
